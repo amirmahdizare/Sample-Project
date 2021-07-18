bf606b2625fd45ac57d87c3e3ecb15138e0f9a0a
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-07-18 10:58:29 +0430

    Add icon to LoginPage

--- src/page/LoginPage/LoginPage.js
+++ src/page/LoginPage/LoginPage.js
@@ -5,9 +5,9 @@ import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
 const LoginPage = () => {
     const [state,setState] = useState({
         username:'',
@@ -15,35 +15,30 @@ const LoginPage = () => {
     })
     const useStyles = makeStyles((theme) => ({
         root: {
-          
+            borderRadius:'10px',
             marginTop: theme.spacing(5),
             padding:'20px',
-            width:'50%',
             display:'flex',
             flexDirection:'column',
             justifyContent:'center',
             boxShadow:'0px 0px 3px gray ',
             textAlign:'center',
             fontSize:'1.5rem',
-            direction:'rtl',
             flexGrow: '1',
-            alignItems:'center'
+            alignItems:'center',
+            backgroundImage: 'linear-gradient(to bottom  , limegreen,white, white )'
           
         },
         input:{
             width:"90%",
-            direction:'rtl',
             float:'right',
-            fontSize:'1em',
-            "& $div":{
-                    right:'0'
-            }
-
-        
-           
+            fontSize:'2em',
+           }
+        ,
+        icon:{
+            fontSize:'6em'
         },
         button:{
-            width:'50%', 
             fontSize:'1em'
         },
         gridContainer:{
@@ -56,25 +51,26 @@ const LoginPage = () => {
       const handleTextFieldChange = (id,value) =>{
           setState({...state,[id]:value})
       }
-      const handleLogIn = () =>{
-          
-      }
+      const handleLogIn = async(e) =>{
+          e.preventDefault()
+        }
     return (
         <>
         <CssBaseline />
        <Container className={classes.root} maxWidth="sm" fixed spacing={2}>
         {/* <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} /> */}
        <form>
-       <h2>ورود به حساب کاربری</h2>
+       <AccountCircleIcon className={classes.icon}   />
+       <h1>Login</h1>
        <Grid  container spacing={6} > 
        <Grid className={classes.gridContainer}  item xs={12} >
-           <TextField id="username" required  variant="outlined" className={classes.input} label="نام کاربری" value={state.username} onChange={(e) => handleTextFieldChange(e.target.id ,e.target.value)}/>
+           <TextField hintStyle={{ width: '600px', textAlign: 'center' }} id="username" required  variant="outlined" className={classes.input} label="Username" value={state.username} onChange={(e) => handleTextFieldChange(e.target.id ,e.target.value)}/>
         </Grid>
         <Grid className={classes.gridContainer} item xs={12}>
-            <TextField type="password" required variant="outlined" id="password" className={classes.input}  label="رمز" value={state.password} onChange={(e) => handleTextFieldChange(e.target.id,e.target.value)} />
+            <TextField type="password" required variant="outlined" id="password" className={classes.input}  label="Password" value={state.password} onChange={(e) => handleTextFieldChange(e.target.id,e.target.value)} />
             </Grid>
         
-        <Grid className={classes.gridContainer} item xs={12}> <Button type="submit" onClick={handleLogIn} className={classes.button}   variant="contained" color="primary" sm={12}>ورود</Button></Grid>
+        <Grid className={classes.gridContainer} item xs={12}> <Button type="submit" onClick={handleLogIn} className={classes.button}   variant="contained" color="secondary" >Sign in</Button></Grid>
 
       
         </Grid>
